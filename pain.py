5bd46be7bcb089873546c79d80dfa665570a1149
lucaped <lucapedersen2008@gmail.com>
2024-10-17 20:57:18 +0000

    first commit

---
+++ pain.py
@@ -0,0 +1,13 @@
+name= "Luca"
+print(name)
+number=1
+booleano=True
+booleano=False
+print(number)
+print(1)
+edaddeLuca=15
+edadparavotar=16
+puedevotar= edadparavotar<=edaddeLuca
+if puedevotar:
+    print("puede votar")
+    
---
+++ while.py
@@ -0,0 +1,6 @@
+contador=0
+condicion=True
+while condicion:
+    contador=contador+1
+    condiion=contador<10
+    print(contador)
